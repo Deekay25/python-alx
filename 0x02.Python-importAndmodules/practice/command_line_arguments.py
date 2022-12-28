77ad9e1bb6389adbe4a5a47a22cec29a1af58661
adnan ahmad <ahmadadnan2235@gmail.com>
2022-12-28 02:28:24 +0100

    boom

--- 0x02.Python-importAndmodules/practice/command_line_arguments.py
+++ 0x02.Python-importAndmodules/practice/command_line_arguments.py
@@ -1,7 +1,9 @@
+# source: https://docs.python.org/3/tutorial/stdlib.html#command-line-arguments
+
 # 10.1. Operating System Interface
 # The os module provides dozens of functions for interacting with the operating system:
 import os
-print(os.getcwd())
+print(os.getcwd()) # returns current working directory
 
 os.chdir('/server/accesslogs')   # Change current working directory
 os.system('mkdir today')   # Run the command mkdir in the system shell
--- delete.py
+++ delete.py
@@ -1,3 +1,4 @@
 print(list(range(10, 0, 2)))
 print("new",list(range(0, -10, -2)))
 print("{:d} Mission street, {}".format(972, "San Francisco"))
+
