19cdf4dbaf4add43d13a710d40e26af09f41b2b1
adnan ahmad <ahmadadnan2235@gmail.com>
2023-01-03 16:47:56 +0100

    small num

---
+++ python-4-e-book/ch5-Iteration/loop-idioms/printing-smallest-number.py
@@ -0,0 +1,16 @@
+# the program is wrong  because smallest so far will always be -1
+# smaller_so_far = -1
+
+# for num in [1,10,22,45,73,27]:
+#     if num < smaller_so_far:
+#         smaller_so_far = num
+#     print(smaller_so_far, num) 
+
+
+smallest_so_far = None
+
+for num in [1,10,22,45,73,27]:
+    if smallest_so_far is None or num < smallest_so_far:
+        smallest_so_far = num
+    print(num, smallest_so_far)
+print(smallest_so_far)
