e9d90036335132b5fd5bc0dcfb7f224723628883
adnan ahmad <ahmadadnan2235@gmail.com>
2023-01-05 00:37:53 +0100

    blah

--- python-4-e-book/ch6-strings/ex5.py
+++ python-4-e-book/ch6-strings/ex5.py
@@ -4,19 +4,24 @@
 # colon character and then use the float function to convert the extracted
 # string into a floating point number.
 
-str = 'X-DSPAM-Confidence:0.8475'
-search = str.find(':')
-slice = str[search+1:]
-slice_fl = float(slice)
-print(slice_fl)
+# str = 'X-DSPAM-Confidence:0.8475'
+# search = str.find(':')
+# slice = str[search+1:]
+# slice_fl = float(slice)
+# print(slice_fl)
 
-#print uct.ac.za
+# #print uct.ac.za
+# data = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
+# d_search = data.find('@')
+# n_search = data.find(' ', d_search)
+# slice_data = data[d_search + 1:n_search]
+# print(slice_data)
+
+# did it myself
+# print '09:14:16'
 data = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
-d_search = data.find('@')
-n_search = data.find(' ', d_search)
-slice_data = data[d_search + 1:n_search]
-print(slice_data)
-
-b_search = data.find('5', ' ')
-c_search = data.find(' ', '6')
-sl_data = data[b_search + 1:c_search]
+a_search = data.find('5')
+b_search = data.find(' ', a_search + 2)
+# print(data[b_search:])
+print(data[a_search+2:b_search])
+# print(a_search)
