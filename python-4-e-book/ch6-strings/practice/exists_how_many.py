4242bfcdcecd4b64d5b272f7dcae885d2219423f
adnan ahmad <ahmadadnan2235@gmail.com>
2023-01-04 02:10:35 +0100

    how many a's

---
+++ python-4-e-book/ch6-strings/practice/exists_how_many.py
@@ -0,0 +1,23 @@
+# write a simple loop that loops through each letter in a
+# strng and count the number of times the loop encounters 'a'
+# character
+# --> Count How many times a exits below
+# --> use both while loop and for loop
+
+# fruit = 'banana'
+# count = 0
+# for i in fruit:
+#     if i == 'a':
+#         count = count + 1
+# print(count)
+
+#while loop
+
+fruit = 'banana'
+count = 0
+index = 0
+while index < len(fruit):
+    if fruit[index] == 'a':
+        count = count + 1
+    index = index + 1
+print(count)
