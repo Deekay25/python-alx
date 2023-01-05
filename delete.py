b5bf5b5792d2a29076e9f9ee8531a5b8a70c3e1f
adnan ahmad <ahmadadnan2235@gmail.com>
2023-01-05 03:42:59 +0100

    bull

--- delete.py
+++ delete.py
@@ -2,3 +2,14 @@ print(list(range(10, 0, 2)))
 print("new",list(range(0, -10, -2)))
 print("{:d} Mission street, {}".format(972, "San Francisco"))
 
+l1 = [1, 2, 3]
+l2 = l1
+l1.append(4)
+print(l2)
+a = [1,2]
+b = a + [3]
+print(b)
+
+a = (1, 2)
+b = (1, 2)
+print(a is b)
