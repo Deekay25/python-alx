a08b27f619dc1ee1f48ca7be4374468153afd8ec
adnan ahmad <ahmadadnan2235@gmail.com>
2023-01-06 00:59:17 +0100

    gettign difficult

--- python-4-e-book/ch8-lists/ex1.py
+++ python-4-e-book/ch8-lists/ex1.py
@@ -2,3 +2,17 @@
 # it, removing the first and last elements, and returns None. Then write
 # a function called middle that takes a list and returns a new list that
 # contains all but the first and last elements.
+
+def chop(lst)->list:
+    new_lst = lst[1:-1]
+    return new_lst
+
+print(chop([1,2,3,5,6,7,8]))
+
+
+def middle(lst)->list:
+    new_list = lst[:]
+    del new_list[1:-1]
+    return new_list
+
+print(middle([1,2,4,5,6]))
--- python-4-e-book/ch8-lists/practice/parsing_lines.py
+++ python-4-e-book/ch8-lists/practice/parsing_lines.py
@@ -5,17 +5,35 @@
 # Fri
 # Fri
 
-fhand = open('mbox-short.txt')
-for line in fhand:
-    line = line.rstrip()
-    if line.startswith('From '):
-        line = line.split()
-        print(line[2])
+# fhand = open('mbox-short.txt')
+# for line in fhand:
+#     line = line.rstrip()
+#     if line.startswith('From '):
+#         line = line.split()
+#         print(line[2])
 
-#better approach
+# #better approach
+# fhand = open('mbox-short.txt')
+# for line in fhand:
+#     line = line.rstrip()
+#     if not line.startswith('From '): continue
+#     line = line.split()
+#     print(line[2])
+
+#another way
+
+# fhand = open('mbox-short.txt')
+# for line in fhand:
+#     line = line.split()
+#     print('debug',line)
+#     if line[0] != 'From ': continue //this fialed when it reaches an empty line
+#     print(line[2])
+
+# solution to above fail
 fhand = open('mbox-short.txt')
 for line in fhand:
-    line = line.rstrip()
-    if not line.startswith('From '): continue
     line = line.split()
+    # print('debug',line)
+    if len(line) == 0 : continue
+    if line[0] != 'From': continue
     print(line[2])
