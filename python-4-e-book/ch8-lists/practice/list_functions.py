831c7035bd116e1b76e0867eeebb17841015e3e1
adnan ahmad <ahmadadnan2235@gmail.com>
2023-01-05 03:30:01 +0100

    half done with lists

---
+++ python-4-e-book/ch8-lists/practice/list_functions.py
@@ -0,0 +1,6 @@
+"""
+write a program that takes numbers as input
+from the user then calculate the average of the numbers 
+using sum(),len() functions. Numbers are to be stored 
+in a list. if a user enters 'done', program terminates
+"""
