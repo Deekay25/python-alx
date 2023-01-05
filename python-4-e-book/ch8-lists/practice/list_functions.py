769a17ec74eca7a3004a4d2ff5344c10b3d27259
adnan ahmad <ahmadadnan2235@gmail.com>
2023-01-05 23:37:12 +0100

    practice done

--- python-4-e-book/ch8-lists/practice/list_functions.py
+++ python-4-e-book/ch8-lists/practice/list_functions.py
@@ -4,3 +4,13 @@ from the user then calculate the average of the numbers
 using sum(),len() functions. Numbers are to be stored 
 in a list. if a user enters 'done', program terminates
 """
+
+num = list()
+
+while True:
+    inp = input('Enter the number: ')
+    if inp == 'done':
+        break
+    f_inp = float(inp)
+    num.append(f_inp)
+print(sum(num)/len(num))
--- python-4-e-book/ch8-lists/practice/parsing_lines.py
+++ python-4-e-book/ch8-lists/practice/parsing_lines.py
@@ -5,3 +5,17 @@
 # Fri
 # Fri
 
+fhand = open('mbox-short.txt')
+for line in fhand:
+    line = line.rstrip()
+    if line.startswith('From '):
+        line = line.split()
+        print(line[2])
+
+#better approach
+fhand = open('mbox-short.txt')
+for line in fhand:
+    line = line.rstrip()
+    if not line.startswith('From '): continue
+    line = line.split()
+    print(line[2])
