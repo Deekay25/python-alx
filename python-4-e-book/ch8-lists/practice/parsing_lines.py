e4f4d61534d0ec805e34af25fc2d574fd577e267
adnan ahmad <ahmadadnan2235@gmail.com>
2023-01-06 02:05:19 +0100

    update parsing of line

--- python-4-e-book/ch8-lists/practice/parsing_lines.py
+++ python-4-e-book/ch8-lists/practice/parsing_lines.py
@@ -29,11 +29,41 @@
 #     if line[0] != 'From ': continue //this fialed when it reaches an empty line
 #     print(line[2])
 
-# solution to above fail
+# solution to above immediate program
+# fhand = open('mbox-short.txt')
+# for line in fhand:
+#     line = line.split()
+#     print('debug',line)
+#     if len(line) == 0 : continue
+#     if line[0] != 'From': continue
+#     print(line[2])
+
+# Exercise 2: Figure out which line of the above program is still not
+# properly guarded. See if you can construct a text file which causes the
+# program to fail and then modify the program so that the line is properly
+# guarded and test it to make sure it handles your new text file.
+
+# for exercise 1, it cannot handle properly when a file liine contains only from 
+# therefore, the below program fixes that 
+# the below program is important to understand
+fhand = open('exercise8_2.txt')
+for line in fhand:
+    words = line.split()
+
+    if len(words) < 3:
+        continue
+    if words[0] != 'From':
+        continue
+    print(words[2])
+
+# Exercise 3: Rewrite the guardian code in the above example without
+# two if statements. Instead, use a compound logical expression using
+# the or logical operator with a single if statement.
 fhand = open('mbox-short.txt')
 for line in fhand:
-    line = line.split()
-    # print('debug',line)
-    if len(line) == 0 : continue
-    if line[0] != 'From': continue
-    print(line[2])
+    words = line.split()
+
+    if len(words) < 3 or words[0] != 'From':
+        continue
+    print(words[2])
+    
