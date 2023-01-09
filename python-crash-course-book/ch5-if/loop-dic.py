c56b1a3dfdddc1e059467190d1b242f44df85921
adnan ahmad <ahmadadnan2235@gmail.com>
2023-01-09 22:17:58 +0100

    looping through a dictionary

---
+++ python-crash-course-book/ch5-if/loop-dic.py
@@ -0,0 +1,13 @@
+user_0 = {
+'username': 'efermi',
+'first': 'enrico',
+'last': 'fermi',
+}
+
+#best way to do it
+for key, value in user_0.items():
+    print(key, value)
+
+# practicing
+for v in user_0:
+    print(v,user_0[v])
