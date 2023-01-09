7b70c9a975ffdcd18296a8517302e01fca21aae4
adnan ahmad <ahmadadnan2235@gmail.com>
2023-01-09 19:58:38 +0100

    dic and files

--- python-4-e-book/ch9-dictionaries/dictionary-and-files.py
+++ python-4-e-book/ch9-dictionaries/dictionary-and-files.py
@@ -1,1 +1,14 @@
-# write a program that counts each word and number of occurence from a file called romeo.txt
+# write a program that counts each word and number of occurence from a file called romeo.txt 
+# and save it to a dictionary
+
+fhand = open('romeo.txt')
+dic = dict()
+for line in fhand:
+    words = line.split()
+    for word in words:
+        if word not in dic:
+            dic[word] = 1
+        else:
+            dic[word] = dic[word] + 1
+print((dic))
+
