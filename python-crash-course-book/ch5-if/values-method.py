d5e1df0b99f7747e6e83e71cf44b90969ce2d30b
adnan ahmad <ahmadadnan2235@gmail.com>
2023-01-09 22:34:08 +0100

    values method

---
+++ python-crash-course-book/ch5-if/values-method.py
@@ -0,0 +1,13 @@
+favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'ruby',
+'phil': 'python',
+}
+
+for language in favorite_languages.values():
+    if 'python' in language:
+        print ('True')
+    else:
+        print('false')
+    
