aafb509bf9f0cf10e8e2d15dc49e6160a6f64197
adnan ahmad <ahmadadnan2235@gmail.com>
2023-01-13 00:11:40 +0100

    most common words

--- python-4-e-book/ch10-tuples/most-c-words.py
+++ python-4-e-book/ch10-tuples/most-c-words.py
@@ -1,25 +1,32 @@
+# start solving tomorrow
 #print 10 most common words from romeo-full.txt
 # solve with list comprehension as well
+
+# But, soft! what light through yonder window breaks?
+# It is the east, and Juliet is the sun.
+# Arise, fair sun, and kill the envious moon,
+# Who is already sick and pale with grief,
+
 # print( sorted( [ (v,k) for k,v in counts.items() ], reverse=True ) )
-import string
-fhand = open('romeo-full.txt')
-counts = dict()
-for line in fhand:
-    line = line.translate(str.maketrans('', '', string.punctuation))
-    line = line.lower()
-    words = line.split()
-    for word in words:
-        if word not in counts:
-            counts[word] = 1
-        else:
-            counts[word] += 1
-# Sort the dictionary by value
-lst = list()
-for key, val in list(counts.items()):
-    lst.append((val, key))
+# import string
+# fhand = open('romeo-full.txt')
+# counts = dict()
+# for line in fhand:
+#     line = line.translate(str.maketrans('', '', string.punctuation))
+#     line = line.lower()
+#     words = line.split()
+#     for word in words:
+#         if word not in counts:
+#             counts[word] = 1
+#         else:
+#             counts[word] += 1
+# # Sort the dictionary by value
+# lst = list()
+# for key, val in list(counts.items()):
+#     lst.append((val, key))
 
-lst.sort(reverse=True)
+# lst.sort(reverse=True)
 
-#this means give me from 0 to 10 from the list that you have already sorted
-for key, val in lst[:10]:
-    print(key, val)
+# #this means give me from 0 to 10 from the list that you have already sorted
+# for key, val in lst[:10]:
+#     print(key, val)
